582c7607a2488dec5a8bc1235d02640f8be1847c
a.sashina <a.sashina@tinkoff.ru>
2023-10-13 00:03:13 +0300

    Исправление второй лабороторной

--- Forecasting short time series/main.py
+++ Forecasting short time series/main.py
@@ -8,21 +8,15 @@ from statsmodels.tsa.seasonal import seasonal_decompose
 
 def main():
     file_path = pathlib.Path(__file__).with_name('data.dat')
-    data = pd.read_csv(file_path, delimiter="\t", header=None, skiprows=1,
-                       names=['fort', 'dry', 'sweet', 'red', 'rose', 'spark', 'total', 'year_', 'month_', 'date_'])
+    data = pd.read_csv(file_path, delimiter="\t")
 
-    # Временной ряд потребления всех вин
-    time_series = data['total']
+    time_series = data['spark']
 
-    # Даты начала и конца временного ряда
-    # Индекс содержит даты между start_date и end_date с шагом в месяц
     start_date = '01-01-1980'
     end_date = '07-01-1994'
     date_index = pd.date_range(start=start_date, end=end_date, freq='M')
-
     time_series.index = date_index
 
-    # Построение графика исходного временного ряда
     plt.figure(figsize=(12, 6))
     plt.plot(time_series)
     plt.title('Исходный временной ряд')
@@ -30,9 +24,7 @@ def main():
     plt.ylabel('Значение')
     plt.show()
 
-    # Использование метода seasonal_decompose для анализа сезонности(разложения временного ряда на компоненты тренда и сезонности).
-    result = seasonal_decompose(time_series, model='additive', period=12)  # Период сезонности 12 месяцев
-    #график тренда
+    result = seasonal_decompose(time_series, model='additive', period=12)
     plt.figure(figsize=(12, 6))
     plt.subplot(211)
     plt.plot(result.trend)
@@ -40,7 +32,7 @@ def main():
     plt.xlabel('Год')
     plt.ylabel('Значение')
     plt.grid(True)
-    # график сезонности
+
     plt.subplot(212)
     plt.plot(result.seasonal)
     plt.title('Сезонность')
@@ -54,17 +46,14 @@ def main():
     p, d, q = 1, 1, 1  # Параметры ARIMA (порядок авторегрессии, интеграции, скользящего среднего)
     P, D, Q, s = 1, 1, 1, 12  # Параметры сезонности
 
-    # Объект SARIMA обучается на данных временного ряда.
     model = SARIMAX(time_series, order=(p, d, q), seasonal_order=(P, D, Q, s))
     results = model.fit()
 
-    # Выполнение прогноза на 8 месяцев вперед
-    forecast_steps = 8
+    # Выполнение прогноза на 12 месяцев вперед
+    forecast_steps = 12
     forecast = results.get_forecast(steps=forecast_steps)
-
     forecast_conf_int = forecast.conf_int()
 
-    # Построение графиков исходного ряда и прогноза
     plt.figure(figsize=(12, 6))
     plt.plot(time_series, label='Исходный ряд', color='blue')
     plt.plot(forecast.predicted_mean, label='Прогноз', color='red')
@@ -77,9 +66,8 @@ def main():
     plt.grid(True)
     plt.show()
 
-    # Вывод спрогнозированных значений
     forecast_values = forecast.predicted_mean[-forecast_steps:]
-    print('Спрогнозированные значения на 8 месяцев вперед:')
+    print('Спрогнозированные значения на 12 месяцев вперед:')
     print(forecast_values)
 
 
